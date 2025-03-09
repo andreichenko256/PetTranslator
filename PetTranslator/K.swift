0ec539996872511648ff0b4f318242e2f2655f2a
andreichenko256 <andreichenko.kirill2000@gmail.com>
2025-03-10 01:05:34 +0200

    Fix folder structure

--- PetTranslator/K.swift
+++ PetTranslator/K.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 struct K {
-    struct Colors {
+    struct Colors{
         static let mainGradient: [CGColor] = [
             UIColor(hex: "#F3F5F6").cgColor,
             UIColor(hex: "#C9FFE0").cgColor
