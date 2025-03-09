0a9427aaa929ea71f673fa2011214e75f2e5a045
andreichenko256 <andreichenko.kirill2000@gmail.com>
2025-03-09 23:50:01 +0200

    Initial Commit

---
+++ PetTranslator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetTranslator
+//
+//  Created by Kyrylo on 09.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
